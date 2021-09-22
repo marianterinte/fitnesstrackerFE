d3545e11572e405f6e37241b6696ebe31d2cb13e
Marian Terinte <marian.terinte@gmail.com>
2021-09-22 13:20:24 +0300

    added spinner partial

--- FitnessTracker/src/app/training/new-training/new-training.component.ts
+++ FitnessTracker/src/app/training/new-training/new-training.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-new-training',
-  templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  selector: "app-new-training",
+  templateUrl: "./new-training.component.html",
+  styleUrls: ["./new-training.component.css"],
 })
 export class NewTrainingComponent implements OnInit {
 
-  constructor() { }
+  @Output() startOnGoingTrainingEvent =  new EventEmitter<void>();
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  startOnGoingTraining() {
+    this.startOnGoingTrainingEvent.emit();
   }
-
 }
